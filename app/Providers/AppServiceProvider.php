9bd74fd23a1c8cc127a22a8b07f38403ab59ffae
sahrulraiya23 <muhammadsahrulraiya23@gmail.com>
2025-09-09 18:36:58 +0800

    fix service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,7 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
 
 
-    use Illuminate\Support\Facades\URL;
 
     public function boot()
     {
